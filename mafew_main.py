bf2bc6bfdc45559256693e97daa1aa72c040cb41
Ivanov-M <ivanovm@cf.ac.uk>
2022-11-15 14:47:33 +0200

    ignore me

--- mafew_main.py
+++ mafew_main.py
@@ -9,8 +9,12 @@ import pandas as pd
 import os
 
 from matplotlib import pyplot as plt
-from sklearn.feature_selection import RFECV, SelectKBest, chi2
-from sklearn.metrics import accuracy_score, ConfusionMatrixDisplay, confusion_matrix, classification_report
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.feature_selection import RFECV, SelectKBest, chi2, mutual_info_regression, f_regression, SelectFromModel, \
+    f_classif
+from sklearn.linear_model import Lasso
+from sklearn.metrics import accuracy_score, ConfusionMatrixDisplay, confusion_matrix, classification_report, r2_score, \
+    mean_absolute_error
 from sklearn.model_selection import train_test_split, StratifiedKFold
 from tqdm import tqdm
 import xgboost as xgb
@@ -59,12 +63,14 @@ class Conditions(Enum):
 def save_factor(factor):
     if isinstance(factor, Diseases):
         data_path = os.path.join(DATA_PATH, "disease_methylation_v1.txt")
+        save_path = os.path.join(DATA_PATH, "diseases")
         names = pd.read_csv(data_path, sep='\t', nrows=2)
         names = names.to_numpy().squeeze()
         columns = np.concatenate((np.array([0]),
                                   np.argwhere((names[0] == factor.value) & (names[1] == "whole blood")).squeeze()))
     elif isinstance(factor, Conditions):
         data_path = os.path.join(DATA_PATH, factor.value + "_methylation.txt")
+        save_path = os.path.join(DATA_PATH, "conditions")
         names = pd.read_csv(data_path, sep='\t', nrows=2)
         names = names.to_numpy().squeeze()
         columns = np.concatenate((np.array([0]),
@@ -73,29 +79,12 @@ def save_factor(factor):
         return
     if columns.size < 10:
         return
-    if factor.value == "age":
-        column_chunks = np.array_split(columns, 5)
-    else:
-        column_chunks = np.array_split(columns, 3)
-    for i, col in enumerate(column_chunks):
-        print(f"Section {i}...")
-        with open(data_path, 'r') as f:
-            reader = csv.reader(f, delimiter="\t")
-            list_of_cgs = []
-            for row in tqdm(reader, total=485000):
-                list_of_cgs.append(np.array(row)[columns].squeeze())
-        to_df = pd.DataFrame(list_of_cgs)
-        del list_of_cgs
-        if isinstance(factor, Diseases):
-            save_path = os.path.join(DATA_PATH, "diseases")
-        elif isinstance(factor, Conditions):
-            save_path = os.path.join(DATA_PATH, "conditions")
-        else:
-            return
-        # os.makedirs(save_path, exist_ok=True)
-        to_df.to_csv(os.path.join(save_path, factor.value + "_{}.csv".format(i)), index=False)
-        del to_df
-
+    with open(data_path, 'r') as datafile, open(os.path.join(save_path, factor.value + ".csv"),
+                                                'w+', newline='') as resultfile:
+        reader = csv.reader(datafile, delimiter="\t")
+        writer = csv.writer(resultfile, delimiter=',')
+        for row in tqdm(reader, total=485000):
+            writer.writerow(np.array(row)[columns].squeeze())
 
 
 def save_condition(condition):
@@ -129,22 +118,21 @@ def clean_factor(factor):
     if isinstance(factor, Diseases):
         path = os.path.join(DATA_PATH, "diseases", factor.value + ".csv")
     elif isinstance(factor, Conditions):
-        path = os.path.join(DATA_PATH, "conditions", factor.value + ".txt")
+        path = os.path.join(DATA_PATH, "conditions", factor.value + ".csv")
     else:
         print("Warning: passed criteria is not recognised.")
         path = os.path.join(DATA_PATH, factor.value + ".csv")
     try:
         if isinstance(factor, Diseases):
-            df = pd.read_csv(path, low_memory=False, nrows=10)
             saving_path = get_disease_path(factor)
         elif isinstance(factor, Conditions):
-            df = pd.read_csv(path, sep='\t', nrows=10)
             saving_path = get_condition_path(factor)
         else:
             saving_path = os.path.join(DATA_PATH, str(factor.value), factor.value + "_clean.csv")
     except FileNotFoundError as FNF:
         print(FNF)
         return
+    df = pd.read_csv(path, low_memory=False)
     with open(os.path.join(saving_path, 'cleaning_info.txt'), 'w+') as f:
         nan_cnt = df.isna().sum().sum()
         f.write(f"Initial data: Total patients were: {df.shape[1] - 1}, \n")
@@ -153,7 +141,7 @@ def clean_factor(factor):
             control = (df.loc[3] == 'control').T.sum()
             f.write(f" with {cases} cases and {control} controls. \n")
         f.write(f"Initial data: Total NaNs were: {nan_cnt}, or {100 * nan_cnt / df.size} % of data. \n")
-        f.write("Initial data: Total CpG number was: {}. \n".format(df["0"].astype("str").str.startswith("ch").size))
+        f.write("Initial data: Total CpG number was: {}. \n".format(df["sample_id"].astype("str").str.startswith("ch").size))
         cpg_nan_thresh = 0.8
         patient_nan_thresh = 0.9
         df = df.dropna(axis=0, thresh=np.ceil(cpg_nan_thresh * (df.shape[1] + 1)).astype(int))  # cpgs
@@ -161,7 +149,7 @@ def clean_factor(factor):
         if df.empty:
             print("Exhausted data.")
             return
-        df = df[~df["0"].astype("str").str.startswith("ch")]
+        df = df[~df["sample_id"].astype("str").str.startswith("ch")]
         nan_cnt = df.isna().sum().sum()
         df = df.fillna(0)
         df.to_csv(os.path.join(saving_path, factor.value + "_clean.csv"), index=False)
@@ -171,10 +159,13 @@ def clean_factor(factor):
             control = (df.loc[3] == 'control').T.sum()
             f.write(f" with {cases} cases and {control} controls. \n")
         f.write(f"Clean data: Total NaNs are: {nan_cnt}, or {100 * nan_cnt / df.size} % of data. \n")
-        f.write("Clean data: Total CpG number is: {}. \n".format(df["0"].astype("str").str.startswith("ch").size))
+        f.write("Clean data: Total CpG number is: {}. \n".format(df["sample_id"].astype("str").str.startswith("ch").size))
     with open(os.path.join(saving_path, 'dataset_info.json'), "w+") as d:
-        info_as_dict = {"cases": int(cases),
-                        "controls": int(control)}
+        if isinstance(factor, Diseases):
+            info_as_dict = {"cases": int(cases),
+                            "controls": int(control)}
+        else:
+            info_as_dict = {"total_patients": df.shape[1] - 1}
         json.dump(info_as_dict, d, indent=4)
 
 
@@ -186,7 +177,12 @@ def load_raw(disease):
 
 
 def load_clean(disease):
-    path = get_disease_path(disease)
+    if isinstance(disease, Diseases):
+        path = get_disease_path(disease)
+    elif isinstance(disease, Conditions):
+        path = get_condition_path(disease)
+    else:
+        return
     if path is None:
         return
     data_loc = os.path.join(path, disease.value + "_clean.csv")
@@ -214,7 +210,7 @@ def create_disease_path(disease):
     os.makedirs(folder, exist_ok=True)
 
 
-def prepare_dataset(df, test_split=0.2):
+def prepare_disease(df, test_split=0.2):
     df.columns = df.loc["sample_id"]  # should have saved with index=False when cleaning data, I think
     df = df[1:]
     X = df.drop(["disease", "tissue", "sample_type"], axis=0).transpose()
@@ -230,6 +226,22 @@ def prepare_dataset(df, test_split=0.2):
     # fit model no training data
 
 
+def prepare_condition(df, test_split=0.2):
+    # df.columns = df.loc["sample_id"]  # should have saved with index=False when cleaning data, I think
+    # df = df[1:]
+    if "age" in df.index:
+        X = df.drop(["age", "tissue"], axis=0).transpose()
+        y = df.loc["age"].astype(float)
+    elif "bmi" in df.index:
+        X = df.drop(["bmi", "tissue"], axis=0).transpose()
+        y = df.loc["bmi"].astype(float)
+    else:
+        raise ValueError
+    X = X.astype(float)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_split)
+    return X_train, X_test, y_train, y_test
+
+
 def find_n_features(begin, minimum, step):
     assert 0 < step < 1
     n_features = [begin]
@@ -239,7 +251,7 @@ def find_n_features(begin, minimum, step):
     return n_features
 
 
-def create_xgb_model(disease, save=True):
+def create_xgb_classifier(disease, save=True):
     """ Prepare dataset: """
     folder = None
     model_path = None
@@ -252,22 +264,12 @@ def create_xgb_model(disease, save=True):
     if dataset is None:
         return
     print("Loaded Dataset...")
-    X_train, X_test, y_train, y_test = prepare_dataset(dataset, 0.25)
+    X_train, X_test, y_train, y_test = prepare_disease(dataset, 0.25)
     del dataset
     """ Select model: """
     model = xgb.XGBClassifier()
     """Perform feature selection: """
-    minimum_features = 25
-    # init_features = X_train.shape[0]
-    # step = 0.1
-    # feature_selector = RFECV(
-    #     estimator=model,
-    #     step=step,
-    #     cv=StratifiedKFold(4),
-    #     scoring="accuracy",
-    #     min_features_to_select=minimum_features,
-    # )
-    initial_feature_selector = SelectKBest(chi2, k=8000)
+    initial_feature_selector = SelectKBest(f_classif, k=8000)
     initial_feature_selector.fit(X_train, y_train)
     feature_idx = initial_feature_selector.get_support(indices=True)
     feature_names = X_train.columns[feature_idx]
@@ -289,15 +291,6 @@ def create_xgb_model(disease, save=True):
     X_test = pd.DataFrame(feature_selector.transform(np.array(X_test)), index=X_test.index, columns=feature_names)
     print("Optimal number of features : %d" % feature_selector.n_features_)
     if save:
-        # Plot number of features VS. cross-validation scores
-        # featuring_fig, featuring_ax = plt.subplots()
-        # featuring_ax.set_xlabel("Number of features selected")
-        # featuring_ax.set_ylabel("Cross validation score (accuracy)")
-        # featuring_ax.plot(
-        #     range(len(rfecv.grid_scores_)),
-        #     rfecv.grid_scores_,
-        # )
-        # featuring_fig.savefig(os.path.join(folder, disease.value + "_feature_selection.png"))
         pd.Series(feature_names, name="CpG_sites").to_csv(
             os.path.join(folder, disease.value + "_selected_features.csv"), index=False)
     """Fit and save final model with stats: """
@@ -327,11 +320,75 @@ def create_xgb_model(disease, save=True):
     plt.close()
 
 
+def create_xgb_regressor(disease, save=True):
+    """ Prepare dataset: """
+    folder = None
+    model_path = None
+    if save:
+        folder = get_condition_path(disease)
+        if folder is None:
+            return
+        model_path = os.path.join(folder, disease.value + "_xgb.json")
+    dataset = load_clean(disease)
+    if dataset is None:
+        return
+    print("Loaded Dataset...")
+    X_train, X_test, y_train, y_test = prepare_condition(dataset, 0.25)
+    del dataset
+    """ Select model: """
+    model = xgb.XGBRegressor()
+    """Perform feature selection: """
+    initial_feature_selector = SelectKBest(f_classif, k=8000)
+    initial_feature_selector.fit(X_train, y_train)
+    feature_idx = initial_feature_selector.get_support(indices=True)
+    feature_names = X_train.columns[feature_idx]
+    X_train = pd.DataFrame(initial_feature_selector.transform(X_train),
+                           index=X_train.index, columns=feature_names)
+    X_test = pd.DataFrame(initial_feature_selector.transform(X_test),
+                          index=X_test.index, columns=feature_names)
+
+    feature_selector = BorutaPy(
+        estimator=model,
+        n_estimators='auto',
+        max_iter=100  # number of trials to perform
+    )
+    feature_selector.fit(np.array(X_train), np.array(y_train))
+    #  Keep CpG names:
+    # feature_idx = feature_selector.get_support(indices=True)
+    feature_names = X_train.columns[feature_selector.support_]
+    X_train = pd.DataFrame(feature_selector.transform(np.array(X_train)), index=X_train.index, columns=feature_names)
+    X_test = pd.DataFrame(feature_selector.transform(np.array(X_test)), index=X_test.index, columns=feature_names)
+    print("Optimal number of features : %d" % feature_selector.n_features_)
+    if save:
+        pd.Series(feature_names, name="CpG_sites").to_csv(
+            os.path.join(folder, disease.value + "_selected_features.csv"), index=False)
+    """Fit and save final model with stats: """
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    predictions = [round(value) for value in y_pred]
+    # evaluate predictions
+    accuracy = r2_score(y_test, predictions)
+    mae = mean_absolute_error(y_test, predictions)
+    print("Accuracy of {} model: {}".format(disease.value, accuracy))
+    print("MAE of {} model: {}".format(disease.value, mae))
+    features_fig, features_ax = plt.subplots()
+    xgb.plot_importance(model, ax=features_ax, max_num_features=20)
+    if save:
+        model.save_model(model_path)
+        features_fig.savefig(os.path.join(folder, disease.value + "_feature_importance_xgb.png"))
+        with open(os.path.join(folder, disease.value + "_xgb_report.json"), "w") as report:
+            json.dump({"mae": mae,
+                       "r2_score": accuracy}, report, indent=4)
+    else:
+        plt.show()
+    plt.close()
+
+
 def main_training_loop():
     for disease in Diseases:
         print(f"Training model for {disease.name}...")
         t1 = time.time()
-        create_xgb_model(disease)
+        create_xgb_classifier(disease)
         print(f"Finished in {timedelta(seconds=time.time() - t1)}. ")
 
 
@@ -381,5 +438,5 @@ def plots_from_info():
 
 
 if __name__ == '__main__':
-    for condition in Conditions:
-        save_factor(condition)
+    for condition in [Conditions.BMI]:
+        create_xgb_regressor(condition)
